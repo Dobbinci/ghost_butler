6be7a494558d06462507b2f61df98126043dbda7
JeongSuA0724 <22000672@handong.ac.kr>
2023-12-04 19:02:16 +0900

    feat: uid로 대화 기록 삭제 기능 구현

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -41,9 +43,20 @@ class _SettingPageState extends State<SettingPage> {
                     content: Text('대화 기록을 삭제하시겠습니까?'),
                     actions: [
                       TextButton(
-                        onPressed: () {
+                        onPressed: () async {
                           // 삭제 로직 추가
-                          // 예: 데이터베이스에서 대화 기록 삭제
+                          User? user = FirebaseAuth.instance.currentUser;
+                          if (user != null) {
+                            await FirebaseFirestore.instance
+                                .collection('messages')
+                                .where('userId', isEqualTo: user.uid)
+                                .get()
+                                .then((QuerySnapshot querySnapshot) {
+                              querySnapshot.docs.forEach((doc) {
+                                doc.reference.delete();
+                              });
+                            });
+                          }
                           Navigator.pop(context);
                         },
                         child: Text('확인'),
@@ -77,8 +90,6 @@ class _SettingPageState extends State<SettingPage> {
               onChanged: (value) {
                 setState(() {
                   pushNotificationEnabled = value;
-                  // 여기에 푸시 알림 활성화 여부에 따른 로직 추가
-                  // 예: 푸시 알림 API 호출, 설정값 저장 등
                 });
               },
             ),
