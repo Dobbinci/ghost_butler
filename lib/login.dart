6bba448be494e8dbc0f2ab0384ec84f17e120ded
Vinci Lee <davinlsj@gmail.com>
2023-12-05 15:04:48 +0900

    q

--- lib/login.dart
+++ lib/login.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ghost_butler/singup.dart';
+import 'package:ghost_butler/signup.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:lottie/lottie.dart';
 
